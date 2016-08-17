748f85e1df6bd890f272dfa5695f8702f3861968
ItsDavidThai <itsdavidthai@gmail.com>
2016-08-17 02:09:19 -0700

    note

---
+++ programmingNotes.js
@@ -0,0 +1,22 @@
+////$(function(){}) is a jquery shortcut for document.on('ready')
+// wrapping an app within a object
+chatBox = {
+    // declare props and methods
+    server: 127.0.0.1,
+
+    init()=>{
+        
+    },
+    processData: () =>{
+
+    },
+    addRooms: () =>{
+        
+    },
+    addMessages: () => {
+
+    },
+    sendMessages: () => {
+
+    }
+}
